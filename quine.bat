adf929d67b0001086051b50efe779df5873e0409
Raattis <Raattis@users.noreply.github.com>
2023-01-25 23:58:32 +0200

    Even less bat

--- quine.bat
+++ quine.bat
@@ -1,33 +1,45 @@
-@goto build
-:build
+@goto bootstrap_builder
+#error Remember to insert "#if 0" into the compiler input pipe or skip the 3 first lines when compiling this file.
+#endif // GOTO_BOOTSTRAP_BUILDER
+
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef BOOTSTRAP_BUILDER
+:bootstrap_builder
 @echo off
+set compiler_exe=tcc.exe
 @rem if exist .\%~n0.exe del .\%~n0.exe
 (
-	echo const char* _source_filename = "%~n0%~x0";
+	echo static const char* b_source_filename = "%~n0%~x0";
+	echo static const char* b_output_exe_filename = "%~n0.exe";
+	echo static const char* b_output_c_filename = "%~n0.c";
+	echo static const char* b_compiler_exe_path = "%compiler_exe%";
+	echo #define BUILDER
 	echo #line 0 "%~n0%~x0"
-	echo #if 0
-	type %~n0%~x0
-) | .\tcc.exe - -DBUILDER -nostdlib -lmsvcrt -nostdinc -Iinclude -Iinclude/winapi -run
-if ERRORLEVEL 1 exit ERRORLEVEL
-.\%~n0.exe
+	echo #if GOTO_BOOTSTRAP_BUILDER
+	type %~n0%~x0 
+) | %compiler_exe% - -run -nostdlib -lmsvcrt -nostdinc -Iinclude -Iinclude/winapi -bench
 @exit ERRORLEVEL
-#endif
+#endif // BOOTSTRAP_BUILDER
+
+///////////////////////////////////////////////////////////////////////////////
+
+//#error test error on line 27
 
 #ifdef BUILDER
-const int _variables_set = 0;
-const char* _compiler_bin = ".\\tcc.exe";
-extern const char* _source_filename;
-char* _output_exe;
-char* _output_c;
-int _create_c_file = 1;
-int _create_exe_file = 1;
-int _run_exe = 0;
-int _verbose = 1;
+static const char* b_compiler_arguments = "-Iinclude -Iinclude/winapi -nostdlib -nostdinc -lmsvcrt -lkernel32";
+static const int b_create_c_file = 1;
+static const int b_create_exe_file = 1;
+static const int b_run_after_build = 1;
+static const char* b_run_arguments = "-h";
+static const int b_verbose = 1;
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
+
+#define FATAL(x, ...) do { if (x) break; fprintf(stderr, "%d: ", __LINE__); fprintf(stderr, __VA_ARGS__ ); fprintf(stderr, "\n(%s)\n", #x); void exit(int); exit(1); } while(0)
 
 char buffer[1024 * 1024];
 
@@ -44,9 +56,9 @@ int insert_snippet(const char* start, const char* stop, FILE* infile, FILE* outf
 
 	if (input_filename) {
 		if (input_filename[0] == '"')
-			fprintf(outfile, "#line %d %s\n", line_number, input_filename);
+			fprintf(outfile, "#line %d %s\n", line_number + 1, input_filename);
 		else
-			fprintf(outfile, "#line %d \"%s\"\n", line_number, input_filename);
+			fprintf(outfile, "#line %d \"%s\"\n", line_number + 1, input_filename);
 	}
 
 	if (stop) {
@@ -85,10 +97,6 @@ int create_code_file(const char* start, const char* stop, const char* input_file
 
 	fseek(infile, 0, SEEK_SET);
 
-	//extern int _create_exe_file;;
-	//if (!_create_exe_file)
-	//	fputs("_runmain() { _start(); }\n", outfile);
-
 	fputs("\nconst char* _source_string = \"\"\n", outfile);
 	insert_file_as_string(infile, outfile);
 	fputc(';', outfile);
@@ -99,185 +107,159 @@ int create_code_file(const char* start, const char* stop, const char* input_file
 
 int compile(const char* output_c, const char* output_exe)
 {
-	extern int _create_exe_file;
-	extern const char* _compiler_bin;
-	snprintf(buffer, sizeof(buffer), "%s %s -o %s -Iinclude -Iinclude/winapi -nostdlib -nostdinc -lmsvcrt -lkernel32 %s", _compiler_bin, output_c, output_exe, _create_exe_file ? "" : "-run");
-	// -L. -vv -bench
+	snprintf(buffer, sizeof(buffer), "%s %s -o %s %s %s", b_compiler_exe_path, output_c, output_exe, b_compiler_arguments, b_create_exe_file ? "" : "-run");
 	int result = system(buffer);
-	if (result != 0)
-		fprintf(stderr, "Error while compiling '%s'. Error value: %d\n", output_c, result);
+	FATAL(result == 0, "Error while compiling '%s'. Error value: %d", output_c, result);
 	return result;
 }
 
-FILE* create_compilation_process(const char* output_exe)
+FILE* create_compilation_process()
 {
-	extern int _create_exe_file;
-	extern const char* _compiler_bin;
-	snprintf(buffer, sizeof(buffer), "%s - -o %s -Iinclude -Iinclude/winapi -nostdlib -nostdinc -lmsvcrt -lkernel32 %s", _compiler_bin, output_exe, _create_exe_file ? "" : "-run");
-	// -L. -vv -bench
+	snprintf(buffer, sizeof(buffer), "%s - -o %s %s %s", b_compiler_exe_path, b_output_exe_filename, b_compiler_arguments, b_create_exe_file ? "" : "-run");
 	FILE* compiler_pipe = popen(buffer, "w");
 	if (compiler_pipe)
 		return compiler_pipe;
-	
-	fprintf(stderr, "Couldn't create a compiler process with '%s'.\n", buffer);
-	exit(1);
+
+	FATAL(0, "Couldn't create a compiler process with '%s'.", buffer);
 	return 0;
 }
 
 void crash_handler(int sig)
 {
-	printf("!!!! crash_handler: %d", sig);
-	exit(sig > 0 ? sig : 1);
-}
-
-#include <sys/stat.h>
-#include <windows.h>
-
-char filename_buffer[1024];
-
-void setGlobalVariables()
-{
-	if (_variables_set)
-		return;
-
-	//printf("_source_filename=%s\n", _source_filename);
-
-	struct stat buf;
-	if (stat(_compiler_bin, &buf) < 0)
-	{
-		if (stat(".\\static-tcc.exe", &buf) >= 0)
-			_compiler_bin = ".\\static-tcc.exe";
-		else if (stat(".\\tcc.exe", &buf) >= 0)
-			_compiler_bin = ".\\static-tcc.exe";
-	}
-
-	int filename_size = strlen(_source_filename) + 1;
-	_output_exe = filename_buffer + filename_size;
-	memcpy(_output_exe, _source_filename, filename_size);
-	_output_exe[filename_size - 4] = 'e';
-	_output_exe[filename_size - 3] = 'x';
-	_output_exe[filename_size - 2] = 'e';
-
-	_output_c = filename_buffer + filename_size * 2;
-	memcpy(_output_c, _source_filename, filename_size);
-	_output_c[filename_size - 4] = 'c';
-	_output_c[filename_size - 3] = 0;
-
-	//printf("%s, %s, %s\n", _source_filename, _output_exe, _output_c);
-
-	int _create_exe_file = 1;
-	int _run_exe = 0;
-	int _verbose = 1;
+	FATAL(0, "!!!! crash_handler: %d", sig);
 }
 
 void _start()
 {
-	setGlobalVariables();
-
 	signal(SIGSEGV, crash_handler);
 
-	if (_verbose)
-		printf("Running %s builder.\n", _output_exe);
+	if (b_verbose)
+		printf("Running %s builder.\n", b_output_exe_filename);
 
-	FILE* compiler_pipe = _create_c_file ? 0 : create_compilation_process(_output_exe);
+	FILE* compiler_pipe = b_create_c_file ? 0 : create_compilation_process();
 
-	const char* input_filename = _source_filename && sizeof(_source_filename) > 1 ? _source_filename : 0;
-	const char* output_c = _output_c && sizeof(_output_c) > 1 ? _output_c : 0;
+	const char* input_filename = b_source_filename && sizeof(b_source_filename) > 1 ? b_source_filename : 0;
+	const char* output_c = b_output_c_filename && sizeof(b_output_c_filename) > 1 ? b_output_c_filename : 0;
 	create_code_file("#ifdef SOURCE", "#endif // SOURCE", input_filename, output_c, compiler_pipe);
 
-	int err = compiler_pipe ? 0 : compile(output_c, _output_exe);
+	int err = compiler_pipe ? 0 : compile(output_c, b_output_exe_filename);
 	if (err != 0)
 		exit(err);
-	
+
 	if (compiler_pipe)
 	{
 		err = pclose(compiler_pipe);
-		if (err != 0)
-		{
-			fprintf(stderr, "Failed to close compiler pipe. Error code: %d\n", err);
-			exit(1);
-		}
+		FATAL(err == 0, "Failed to close compiler pipe. Error code: %d", err);
 	}
 
-	extern int _run_exe;
-	if (_run_exe)
+	if (b_run_after_build)
 	{
-		snprintf(buffer, sizeof(buffer), "%s --!compile --!source", _output_exe);
+		FATAL(b_create_exe_file, "Can't run exe if it wasn't built.");
+
+		snprintf(buffer, sizeof(buffer), "%s %s", b_output_exe_filename, b_run_arguments);
 		printf("---------------------------------------\n");
 		int result = system(buffer);
 		printf("---------------------------------------\n");
-		printf("%s returned %d\n", _output_exe, result);
+		printf("%s returned %d\n", b_output_exe_filename, result);
 	}
-	else if (_verbose)
-	{
-		printf("%s builder successfully ended.\n", _output_exe);
-	}
+
+	if (b_verbose)
+		printf("%s builder successfully finished.\n", b_output_exe_filename);
 
 	exit(0);
 }
 void _runmain() { _start(); }
 #endif // BUILDER
 
+///////////////////////////////////////////////////////////////////////////////
+
 #ifdef SOURCE
 #include <stdio.h>
 #include <windows.h>
 
-void _start()
-{
-	void exit(int);
-	char* commandLine = GetCommandLine();
-	printf("Hello, world!\n");
+//#error test error on line 180
 
-	if (strstr(commandLine, "--source"))
+#define FATAL(x, ...) do { if (x) break; fprintf(stderr, "\n(%s)\n", #x); fprintf(stderr, "%d: ", __LINE__); fprintf(stderr, __VA_ARGS__ ); fprintf(stderr, "\n"); void exit(int); exit(1); } while(0)
+
+void handle_commandline_arguments()
+{
+	char* commandLine = GetCommandLine();
+
+	char exe_filename[1024];
+	char bat_filename[1024];
 	{
-		printf("Here's my source (in stderr):\n\"\"\"\n");
-		extern const char* _source_string;
-		fprintf(stderr, "%s", _source_string);
-		printf("\"\"\"\n");
+		int len = 0;
+		int err = sscanf(commandLine, "%s%n", exe_filename, &len);
+		FATAL(err == 1, "Failed to get filename from GetCommandLine(): '%s', err: %d", commandLine, err);
+
+		if (exe_filename[len - 1] == '"')
+		{
+			len -= 2;
+			memcpy(exe_filename, exe_filename+1, len);
+			exe_filename[len] = 0;
+		}
+		char* start = exe_filename;
+		char* head = exe_filename + len;
+		while (head > start && *(head - 1) != '\\' && *(head - 1) != '/')
+			--head;
+		len = strlen(head);
+		memcpy(bat_filename, head, len - 4);
+		memcpy(exe_filename, head, len + 1);
+		sprintf(bat_filename + len - 4, "_new.bat");
 	}
 
-	if (strstr(commandLine, "--builder"))
+	if (strstr(commandLine, " -h") || strstr(commandLine, "help"))
 	{
-		char bat_filename[1024];
-		char* head = bat_filename;
-		int len = 0;
-		int err = sscanf(commandLine, "%s%n", bat_filename, &len);
-		if (err != 1) exit(1);
-		printf("exe_filename:'%s' %d\n", bat_filename, len);
-		if (bat_filename[len-1] == '"')
-		{
-			sprintf(bat_filename + len - 5, "_new.bat");
-			bat_filename[0] = ' ';
-			memcpy(bat_filename, bat_filename + 1, sizeof(bat_filename) - 1);
-		}
-		else
-			sprintf(bat_filename + len - 4, "_new.bat");
-
-		printf("bat_filename:'%s' %d\n", bat_filename, len);
-		
+	}
+	else if (strstr(commandLine, "--print_source"))
+	{
+		extern const char* _source_string;
+		printf("%s", _source_string);
+		void exit(int);
+		exit(0);
+	}
+	else if (strstr(commandLine, "--create_builder"))
+	{
 		FILE* out = fopen(bat_filename, "w");
-		if (!out)
-		{
-			fprintf(stderr, "Failed to write into '%s'", bat_filename);
-			exit(1);
-		}
-		
-		// FIXME: Doesn't work? No errors, just doesn't produce a file.
-		// Probably a problem with "-characters in the filename.
-		// Should discard "-symbols and only use the filename.
+		FATAL(out, "Failed to get filename from GetCommandLine(): '%s'", commandLine);
+
 		extern const char* _source_string;
 		fputs(_source_string, out);
-		//fputs("hi!", out);
-		err = fclose(out);
-		if (err != 0)
-		{
-			fprintf(stderr, "Failed to close file '%s'. Error code: %d", bat_filename, err);
-			exit(1);
-		}
+		int err = fclose(out);
+		FATAL(err == 0, "Failed to close file '%s'. Error code: %d", bat_filename, err);
+		void exit(int);
+		exit(0);
 	}
 
-	printf("The end!\n");
-	exit(0);
+	{
+		printf("\n" "  How to build?");
+		printf("\n" "    1. Get 'Tiny C Compiler' (links below)");
+		printf("\n" "    2. Run `> .\\%s --create_builder` to produce `%s`", exe_filename, bat_filename);
+		printf("\n" "    3. Place it into tcc\\%s", bat_filename);
+		printf("\n" "    4. Run `.\\%s`", bat_filename);
+		printf("\n");
+		printf("\n" "  Tiny C Compiler:");
+		printf("\n" "    tcc is required for the %s to work as intended.", bat_filename);
+		printf("\n" "    Not many compilers have its feature set such as");
+		printf("\n" "    accepting source code via stdin.");
+		printf("\n");
+		printf("\n" "    Windows download: http://download.savannah.gnu.org/releases/tinycc/");
+		printf("\n" "    Source code: https://github.com/Tiny-C-Compiler/mirror-repository");
+		printf("\n" "    Tested with: tcc-0.9.27-win64-bin.zip");
+		printf("\n");
+		printf("\n" "  Flags:");
+		printf("\n" "    --print_source\tPrint the source code this program was built with into stdout.");
+		printf("\n" "    --create_builder\tOutputs a '%s_new.bat' file that can build this executable.", bat_filename);
+		printf("\n\n");
+		void exit(int);
+		exit(0);
+	}
 }
+
+void _start()
+{
+	handle_commandline_arguments();
+}
+
 void _runmain() { _start(); }
 #endif // SOURCE
