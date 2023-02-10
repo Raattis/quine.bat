96ac212a50e78e0ca7c0a5862e6c2cf38341b985
Raattis <Raattis@users.noreply.github.com>
2023-02-10 19:10:14 +0200

    *click* *whrrrrrrrr*

--- quine.bat
+++ quine.bat
@@ -777,12 +777,29 @@ int window_message_handler_impl(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 		}
 		case WM_PAINT:
 		{
+			printf("WM_PAINT");
 			State *state = (State*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
 			if (!state)
 				printf("No state.\n");
 			
 			paint(hWnd, state);
 			break;
+		}
+		case WM_KEYDOWN:
+		{
+			WORD keyFlags = HIWORD(lParam);
+			WORD repeatCount = LOWORD(lParam);
+			if ((keyFlags & KF_REPEAT) != KF_REPEAT)
+			{
+				printf("*click* *whrrrrrrrr*\n");
+				extern int take_screenshot(HWND);
+				take_screenshot(hWnd);
+			}
+			else
+			{
+				printf("Key repeat. Repeat count since last handled message: %d\n", LOWORD(lParam));
+			}
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		case WM_DESTROY:
 		{
@@ -793,8 +810,156 @@ int window_message_handler_impl(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 			// fallthrough
 		}
 		default:
+			printf("%x\n", message);
 			return DefWindowProc(hWnd, message, wParam, lParam);
 	}
+	return 0;
+}
+
+int take_screenshot(HWND hWnd)
+{
+	HDC hdcScreen;
+	HDC hdcWindow;
+	HDC hdcMemDC = NULL;
+	HBITMAP hbmScreen = NULL;
+	BITMAP bmpScreen;
+	DWORD dwBytesWritten = 0;
+	DWORD dwSizeofDIB = 0;
+	HANDLE hFile = NULL;
+	char* lpbitmap = NULL;
+	HANDLE hDIB = NULL;
+	DWORD dwBmpSize = 0;
+
+	// Retrieve the handle to a display device context for the client 
+	// area of the window. 
+	hdcScreen = GetDC(NULL);
+	hdcWindow = GetDC(hWnd);
+
+	// Create a compatible DC, which is used in a BitBlt from the window DC.
+	hdcMemDC = CreateCompatibleDC(hdcWindow);
+
+	if (!hdcMemDC)
+	{
+		MessageBox(hWnd, TEXT("CreateCompatibleDC has failed"), TEXT("Failed"), MB_OK);
+		goto done;
+	}
+
+	// Get the client area for size calculation.
+	RECT rcClient;
+	GetClientRect(hWnd, &rcClient);
+
+	// This is the best stretch mode.
+	SetStretchBltMode(hdcWindow, HALFTONE);
+
+	// The source DC is the entire screen, and the destination DC is the current window (HWND).
+	if (!StretchBlt(hdcWindow,
+		0, 0,
+		rcClient.right, rcClient.bottom,
+		hdcScreen,
+		0, 0,
+		GetSystemMetrics(SM_CXSCREEN),
+		GetSystemMetrics(SM_CYSCREEN),
+		SRCCOPY))
+	{
+		MessageBox(hWnd, TEXT("StretchBlt has failed"), TEXT("Failed"), MB_OK);
+		goto done;
+	}
+
+	// Create a compatible bitmap from the Window DC.
+	hbmScreen = CreateCompatibleBitmap(hdcWindow, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top);
+
+	if (!hbmScreen)
+	{
+		MessageBox(hWnd, TEXT("CreateCompatibleBitmap Failed"), TEXT("Failed"), MB_OK);
+		goto done;
+	}
+
+	// Select the compatible bitmap into the compatible memory DC.
+	SelectObject(hdcMemDC, hbmScreen);
+
+	// Bit block transfer into our compatible memory DC.
+	if (!BitBlt(hdcMemDC,
+		0, 0,
+		rcClient.right - rcClient.left, rcClient.bottom - rcClient.top,
+		hdcWindow,
+		0, 0,
+		SRCCOPY))
+	{
+		MessageBox(hWnd, TEXT("BitBlt has failed"), TEXT("Failed"), MB_OK);
+		goto done;
+	}
+
+	// Get the BITMAP from the HBITMAP.
+	GetObject(hbmScreen, sizeof(BITMAP), &bmpScreen);
+
+	BITMAPFILEHEADER bmfHeader;
+	BITMAPINFOHEADER bi;
+
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = bmpScreen.bmWidth;
+	bi.biHeight = bmpScreen.bmHeight;
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrUsed = 0;
+	bi.biClrImportant = 0;
+
+	dwBmpSize = ((bmpScreen.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpScreen.bmHeight;
+
+	// Starting with 32-bit Windows, GlobalAlloc and LocalAlloc are implemented as wrapper functions that 
+	// call HeapAlloc using a handle to the process's default heap. Therefore, GlobalAlloc and LocalAlloc 
+	// have greater overhead than HeapAlloc.
+	hDIB = GlobalAlloc(GHND, dwBmpSize);
+	lpbitmap = (char*)GlobalLock(hDIB);
+
+	// Gets the "bits" from the bitmap, and copies them into a buffer 
+	// that's pointed to by lpbitmap.
+	GetDIBits(hdcWindow, hbmScreen, 0,
+		(UINT)bmpScreen.bmHeight,
+		lpbitmap,
+		(BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+	// A file is created, this is where we will save the screen capture.
+	hFile = CreateFile(TEXT("captureqwsx.bmp"),
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL, NULL);
+
+	// Add the size of the headers to the size of the bitmap to get the total file size.
+	dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+	// Offset to where the actual bitmap bits start.
+	bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);
+
+	// Size of the file.
+	bmfHeader.bfSize = dwSizeofDIB;
+
+	// bfType must always be BM for Bitmaps.
+	bmfHeader.bfType = 0x4D42; // BM.
+
+	WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)&bi, sizeof(BITMAPINFOHEADER), &dwBytesWritten, NULL);
+	WriteFile(hFile, (LPSTR)lpbitmap, dwBmpSize, &dwBytesWritten, NULL);
+
+	// Unlock and Free the DIB from the heap.
+	GlobalUnlock(hDIB);
+	GlobalFree(hDIB);
+
+	// Close the handle for the file that was created.
+	CloseHandle(hFile);
+
+	// Clean up.
+done:
+	DeleteObject(hbmScreen);
+	DeleteObject(hdcMemDC);
+	ReleaseDC(NULL, hdcScreen);
+	ReleaseDC(hWnd, hdcWindow);
+
 	return 0;
 }
 
@@ -861,6 +1026,8 @@ static void setup(State* state)
 	state->y = 150;
 
 	create_window(state);
+	
+	take_screenshot(state->hWnd);
 	
 	printf("\n\nGo to the `update` function at the bottom of this source file and edit the `state->x` and `state->y` variable assignments or something, and see what happens. :)\n\n");
 }
